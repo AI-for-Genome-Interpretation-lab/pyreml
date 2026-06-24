7ae6bea8960f6990589cac93cb2d60214ab46db3
Almarch <alexandre.marchal@hotmail.com>
2026-06-24 15:36:56 +0200

    Built site for gh-pages

--- pyreml/__init__.py
+++
@@ -1,4 +0,0 @@
-from .MixedModel import MixedModel
-from .GaussianComponents import Random, Residual
-from .larix import larix
-from .kinships import A_genomic, A_pedigree, D_pedigree, prepare_pedigree
